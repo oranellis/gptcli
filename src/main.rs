e66851249b679c9aacdf06a5b3cb0cc5f7f23fd8
Oran Ellis <oran.mcnair@gmail.com>
2024-02-27 12:14:32 +0000

    Added stdin support for nvim integration

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,36 @@ fn remove_backtick_lines(input_text: &str) -> String {
         .join("\n")
 }
 
+fn get_std_in() -> Option<String> {
+
+    extern crate libc;
+
+    use libc::{poll, pollfd, POLLIN};
+    use std::io::{self, Read};
+    use std::os::unix::io::AsRawFd;
+
+    let stdin_fd = io::stdin().as_raw_fd();
+    let mut fds = [pollfd {
+        fd: stdin_fd,
+        events: POLLIN,
+        revents: 0,
+    }];
+
+    let retval = unsafe { poll(fds.as_mut_ptr(), 1, 0) };
+
+    if retval > 0 {
+        let mut input = String::new();
+        io::stdin().read_to_string(&mut input).unwrap();
+        Some(input)
+    } else if retval == 0 {
+        None
+    } else {
+        None
+    }
+}
+
 #[tokio::main]
 async fn main() {
-    // Make sure you have a file named `.env` with the `OPENAI_KEY` environment variable defined!
 
     let key = "OPENAI_API_KEY"; // Replace this with the name of your environment variable
     match env::var(key) {
@@ -27,7 +54,7 @@ async fn main() {
 
     let mut messages = vec![ChatCompletionMessage {
         role: ChatCompletionMessageRole::System,
-        content: Some("You are a helpful assistant operating in an arch linux command line aiming to provide concise responses. When asked to provide code samples, include only the code block with no additional text.".to_string()),
+        content: Some("You are a helpful general assistant operating in an arch linux command line aiming to provide concise responses. When asked to provide code samples, include only the code block with no additional text.".to_string()),
         name: None,
         function_call: None,
     }];
@@ -35,11 +62,16 @@ async fn main() {
     stdout().flush().unwrap();
 
     let collect: Vec<String> = env::args().skip(1).collect();
-    let user_prompt = collect.join(" ");
+    let mut user_promt = collect.join(" ");
+
+    if let Some(value) = get_std_in() {
+        user_promt.push_str("\n");
+        user_promt.push_str(value.as_str());
+    }
 
     messages.push(ChatCompletionMessage {
         role: ChatCompletionMessageRole::User,
-        content: Some(user_prompt),
+        content: Some(user_promt),
         name: None,
         function_call: None,
     });
